212eb93e10147a616c0938a28076a8ae6452e22a
DavidSC <david@wbcboxing.com>
2020-02-07 18:21:24 -0600

    REto 2 Platzi

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home:Scaffold(
         appBar:AppBar(
-          title: Text("Hola mundo"),
+          title: Text("REto 2"),
         ) ,
-        body: Center(
-          child: Text("Hola mundo Platzi"),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/imgs/bruce-lee.jpg'),
+              fit:BoxFit.fill
+            )
+          ),
+
         ),
       )//MyHomePage(title: 'Flutter Demo Home Page'),
     );
